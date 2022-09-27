8a66942c53b7c004384030650347629777165962
0048CM744 <0048CM744@IBM-PF42VHG4>
2022-09-27 13:35:49 +0530

    JIRANo-Test1 : next commit 4

--- src/main/java/com/example/demo/runner/MessageRunner.java
+++ src/main/java/com/example/demo/runner/MessageRunner.java
@@ -1,14 +1,27 @@
 package com.example.demo.runner;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 @Component
 public class MessageRunner implements CommandLineRunner {
 
+	private static Logger log = LoggerFactory.getLogger(MessageRunner.class);
 	@Override
 	public void run(String... args) throws Exception {
 		int a =10;
+		try
+		{
+			log.info("STRATED");
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		
 		System.out.println("HELLO MODIFIED!!"+ a);
+		log.info("END");
 	}
 
 }
