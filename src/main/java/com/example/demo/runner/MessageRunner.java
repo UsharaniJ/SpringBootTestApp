2f0c4f2686377dd0dd7b4de6ae5e4a67aff04153
0048CM744 <0048CM744@IBM-PF42VHG4>
2022-09-27 13:18:14 +0530

    JIRANo-Test1 : next commit

--- src/main/java/com/example/demo/runner/MessageRunner.java
+++ src/main/java/com/example/demo/runner/MessageRunner.java
@@ -9,6 +9,7 @@ public class MessageRunner implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
 		System.out.println("WELCOME TO SPRING BOOT APP!!");
+		System.out.println("HELLO MODIFIED!!");
 	}
 
 }
